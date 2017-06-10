1511a7250e2dc8e503c35e2fc3df4e4ad3db335d
Mazhar Hussain Warsi <warsi.mazhar@gmail.com>
2017-06-11 01:29:22 +0530

    OpenShift release change to 1.5.1

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -604,8 +604,7 @@ fi
 # Initiating installation of OpenShift Container Platform using Ansible Playbook
 echo $(date) " - Installing OpenShift Container Platform via Ansible Playbook"
 
-#runuser -l $SUDOUSER -c "git clone https://github.com/openshift/openshift-ansible /home/$SUDOUSER/openshift-ansible"
-runuser -l $SUDOUSER -c "git clone -b wmhussain-patch-1 https://github.com/wmhussain/openshift-ansible/ /home/$SUDOUSER/openshift-ansible"
+runuser -l $SUDOUSER -c "git clone https://github.com/openshift/openshift-ansible /home/$SUDOUSER/openshift-ansible"
 
 runuser -l $SUDOUSER -c "ansible-playbook openshift-ansible/playbooks/byo/config.yml"
 
